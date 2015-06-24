2be4cf9ba7ca68c50d5e28231f0db8de111c44a0
Manuel Jesus De la Cruz Sotelo <manuel7851@gmail.com>
2015-06-23 23:09:56 -0500

    listo para empezar a poner estilos en la principal

--- apps/principal/models.py
+++ apps/principal/models.py
@@ -1,16 +1,49 @@
 from django.db import models
+from stdimage import StdImageField
 
 
 class Beneficio(models.Model):
+
+    def image_path(self, filename):
+        return "beneficios/{0}/{1}".format(self.descripcion[:50], str(filename))
+
+    def beneficio_imagen_administrador(self):
+        if not self.enunciado_imagen:
+            return None
+
+        imagenHTML = '<img src="/media/{0}" style="width: 150px" />'.format(
+            self.enunciado_imagen)
+
+        return imagenHTML
+
+    beneficio_imagen_administrador.allow_tags = True
+
     descripcion = models.TextField()
+    imagen = StdImageField(upload_to=image_path)
 
     def __str__(self):
         return self.descripcion
 
 
 class Colaborador(models.Model):
-    nombre = models.CharField(max_length=50)
+
+    def image_path(self, filename):
+        return "beneficios/{0}/{1}".format(self.nombre, str(filename))
+
+    def colaborador_imagen_administrador(self):
+        if not self.enunciado_imagen:
+            return None
+
+        imagenHTML = '<img src="/media/{0}" style="width: 150px" />'.format(
+            self.enunciado_imagen)
+
+        return imagenHTML
+
+    colaborador_imagen_administrador.allow_tags = True
+
+    imagen = StdImageField(upload_to=image_path)
     link = models.URLField()
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nombre
--- eligela/settings/base.py
+++ eligela/settings/base.py
@@ -13,15 +13,15 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-# THIRD_PARTY_APPS = ()
+THIRD_PARTY_APPS = (
+    'stdimage',
+)
 
 LOCAL_APPS = (
     'apps.principal',
 )
 
-# INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
-
-INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
